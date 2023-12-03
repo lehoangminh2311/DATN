c841ef7322bbfd92077b6b1855ae2c5d6a448acb
lehoangminh23112001 <lehoangminh23112001adn@gmail.com>
2023-12-03 23:47:26 +0700

    first commit

---
+++ js.js
@@ -0,0 +1,80 @@
+jQuery(document).ready(function ($) {
+    $(".slick-blog").slick({
+        dots: false,
+        infinite: true,
+        autoplaySpeed: 3000,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: false,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 993,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 785,
+                settings: {
+                    slidesToShow: 2,                  
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+});
+
+
+jQuery(document).ready(function ($) {
+    $(".slick-product").slick({
+        dots: false,
+        infinite: true,
+        autoplaySpeed: 3000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        arrows: false,
+        responsive: [         
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+});
+
+
+AOS.init({
+    once: true,
+});
+
+
+/* Menu fixed */
+$(window).scroll(function () {
+    var cach_top = $(window).scrollTop();
+    var heaigt_header = $(".big-menu").height();
+
+    if (cach_top >= heaigt_header) {
+        if (!$(".big-menu").hasClass("fix_head animate__animated animate__fadeIn")) {
+            $(".big-menu").addClass("fix_head animate__animated animate__fadeIn");
+        }
+    } else {
+        $(".big-menu").removeClass("fix_head animate__animated animate__fadeIn");
+    }
+});
+
+
